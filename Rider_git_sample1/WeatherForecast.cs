cf018fa208613094ec9ffde0bffebc8ddeb82fa4
Алексей Дмитроченко <user012@as.local>
2024-11-11 13:58:46 +0700

    Init

---
+++ Rider_git_sample1/WeatherForecast.cs
@@ -0,0 +1,12 @@
+namespace Rider_git_sample1;
+
+public class WeatherForecast
+{
+    public DateTime Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int) (TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
