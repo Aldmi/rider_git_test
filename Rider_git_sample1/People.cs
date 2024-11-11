8e1fe0ec83c162880c4b3dda34ad4b5643b5eb09
Aldmi <Aldmi@bk.ru>
2024-11-11 19:44:38 +0700

    Update People.cs add PostCode

--- Rider_git_sample1/People.cs
+++ Rider_git_sample1/People.cs
@@ -7,4 +7,5 @@ public class People
     public long Age { get; init; }
     public string Email { get; init; }
     public string Address123 { get; init; }
+    public int PostCode { get; init; }
 }
