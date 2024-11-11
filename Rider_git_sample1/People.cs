9749dc801e12733d96675e58026e07b2ab4859c2
Алексей Дмитроченко <user012@as.local>
2024-11-11 18:14:32 +0700

    address1->address123

--- Rider_git_sample1/People.cs
+++ Rider_git_sample1/People.cs
@@ -2,9 +2,9 @@
 
 public class People
 {
-    public Guid Id { get; set; }
-    public string Name { get; set; }
-    public long Age { get; set; }
-    public string Email { get; set; }
-    public string Address1 { get; set; }
+    public Guid Id { get; init; }
+    public string Name { get; init; }
+    public long Age { get; init; }
+    public string Email { get; init; }
+    public string Address123 { get; init; }
 }
